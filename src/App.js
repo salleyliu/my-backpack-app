aa8b6c9700ff50d60bc8b6f13655e5ebbf733629
Salley Liu <salley.liu@skyscanner.net>
2021-11-11 11:09:40 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { BpkCode } from 'bpk-component-code';
-import BpkButton from 'bpk-component-button';
-import BpkText from 'bpk-component-text';
+import React from 'react'
 
-import STYLES from './App.scss';
+import { BpkCode } from 'bpk-component-code'
+import BpkButton from 'bpk-component-button'
+import BpkText from 'bpk-component-text'
+import STYLES from './App.scss'
 
-const c = className => STYLES[className] || 'UNKNOWN';
+// import('./diff/a');
+
+// import $ from 'jquery';
+
+// $('.my-element').animate(/* ... */);
+
+const c = className => STYLES[className] || 'UNKNOWN'
+
+// const url = "http://127.0.0.1:3001/users";
+
+// fetch(url).then(data => {console.log(data)})
+// .then(res => {console.log(res)})
+// .catch(error => {console.log(error)});
 
 const App = () => (
   <div className={c('App')}>
     <header className={c('App__header')}>
       <div className={c('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Welcome to React + Backpack</BpkText>
+        <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>
+          Welcome to React + Backpack
+        </BpkText>
       </div>
     </header>
     <main className={c('App__main')}>
@@ -19,8 +33,10 @@ const App = () => (
         To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
       </BpkText>
       <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
+      {/* <image style={{width:"50%", height:"50%"}} alt="image title" src="https://js.skyscnr.com/images/country/flag/header/uk.png"></image> */}
+      {/* <script src="https://js.skyscnr.com"></script> */}
     </main>
   </div>
-);
+)
 
-export default App;
+export default App
